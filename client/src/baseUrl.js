40d8a91bae99e7d5254bbd435ba1b683abb6b17d
paulpdoa <polopdoandres@gmail.com>
2024-07-06 14:09:34 +0800

    created gitignores

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -2,6 +2,6 @@ export const baseUrl = () => {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
         return 'http://localhost:8080/api';
     } else {
-        return '';
+        return 'https://sms-api-bgpa.onrender.com';
     }
 }
